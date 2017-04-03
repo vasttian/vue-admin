5dda25c46e25abd7b2b0ee173246c37576c30371
tianyiming <tianyiming26@gmail.com>
2017-04-03 13:25:57 +0800

    add login.vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css'
+import Mock from './mock';
+Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css';
+
+Vue.use(ElementUI);
+// Vue.use(Vuex);
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  // NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' });
+  } else {
+    next();
+  };
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +34,5 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+  // render: h => h(App)
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,71 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '../views/Login.vue';
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+let routes = [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    // {
+    //     path: '/404',
+    //     component: NotFound,
+    //     name: '',
+    //     hidden: true
+    // },
+    // //{ path: '/main', component: Main },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '导航一',
+    //     iconCls: 'el-icon-message',//图标样式class
+    //     children: [
+    //         { path: '/main', component: Main, name: '主页', hidden: true },
+    //         { path: '/table', component: Table, name: 'Table' },
+    //         { path: '/form', component: Form, name: 'Form' },
+    //         { path: '/user', component: user, name: '列表' },
+    //     ]
+    // },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '导航二',
+    //     iconCls: 'fa fa-id-card-o',
+    //     children: [
+    //         { path: '/page4', component: Page4, name: '页面4' },
+    //         { path: '/page5', component: Page5, name: '页面5' }
+    //     ]
+    // },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '',
+    //     iconCls: 'fa fa-address-card',
+    //     leaf: true,//只有一个节点
+    //     children: [
+    //         { path: '/page6', component: Page6, name: '导航三' }
+    //     ]
+    // },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: 'Charts',
+    //     iconCls: 'fa fa-bar-chart',
+    //     children: [
+    //         { path: '/echarts', component: echarts, name: 'echarts' }
+    //     ]
+    // },
+    // {
+    //     path: '*',
+    //     hidden: true,
+    //     redirect: { path: '/404' }
+    // }
+];
+
+export default new Router({
+	routes
+});
