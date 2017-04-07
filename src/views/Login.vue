049c0eac7419265ea2503ac1a740b6815879d236
tianyiming <tianyiming26@gmail.com>
2017-04-07 14:09:03 +0800

    add the enter to login function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,13 @@
       <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input
+        type="password"
+        v-model="ruleForm2.checkPass"
+        auto-complete="off"
+        placeholder="密码"
+        @keyup.enter.native="handleSubmit2">
+      </el-input>
     </el-form-item>
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
