92f52ff68a51194851f7bdff3ea026d929852009
tianyiming26@gmail.com <tianyiming26@gmail.com>
2017-08-16 17:26:18 +0800

    mod: The code style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,171 @@
 <template>
-	<el-row class="container">
-		<el-col :span="24" class="header">
-			<el-col :span="10" class="logo" :class="collapsed ? 'logo-collapse-width':'logo-width'">
-			{{collapsed ? '' : sysName}}
+	<el-row
+		class="container">
+		<el-col
+			:span="24"
+			class="header">
+			<el-col
+				:span="10"
+				class="logo"
+				:class="collapsed ? 'logo-collapse-width' : 'logo-width'">
+			{{ collapsed ? '' : sysName }}
 			</el-col>
-			<el-col :span="10">
-				<div class="tools" @click.prevent="collapse">
+			<el-col
+				:span="10">
+				<div
+					class="tools"
+					@click.prevent="collapse">
 					<i class="fa fa-align-justify"></i>
 				</div>
 			</el-col>
-			<el-col :span="4" class="userinfo">
-				<el-dropdown trigger="hover">
-					<span class="el-dropdown-lick userinfo-inner">
+			<el-col
+				:span="4"
+				class="userinfo">
+				<el-dropdown
+					trigger="hover">
+					<span
+						class="el-dropdown-lick userinfo-inner">
 						<!-- <img :src="this.sysUserAvatar" /> {{sysUserName}} -->
-						<img src="../assets/vasttian.png" /> {{sysUserName}}
+						<img src="../assets/vasttian.png" />
+						{{ sysUserName }}
 					</span>
-					<el-dropdown-menu slot="dropdown">
-						<el-dropdown-item>我的消息</el-dropdown-item>
-						<el-dropdown-item>设置</el-dropdown-item>
-						<el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+					<el-dropdown-menu
+						slot="dropdown">
+						<el-dropdown-item>
+							我的消息
+						</el-dropdown-item>
+						<el-dropdown-item>
+							设置
+						</el-dropdown-item>
+						<el-dropdown-item
+							divided
+							@click.native="logout">
+							退出登录
+						</el-dropdown-item>
 					</el-dropdown-menu>
 				</el-dropdown>
 			</el-col>
 		</el-col>
-		<el-col :span="24" class="main">
-			<aside :class="collapsed ? 'menu-collapsed' : 'menu-expanded'">
+		<el-col
+			:span="24"
+			class="main">
+			<aside
+				:class="collapsed ? 'menu-collapsed' : 'menu-expanded'">
+
 				<!--导航菜单-->
-				<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
-					 unique-opened router v-show="!collapsed">
-					<template v-for="(item, index) in $router.options.routes" v-if="!item.hidden">
-						<el-submenu :index="index+''" v-if="!item.leaf">
-							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-							<el-menu-item v-for="child in item.children" :index="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
+				<el-menu
+					:default-active="$route.path"
+					class="el-menu-vertical-demo"
+					@open="handleopen"
+					@close="handleclose"
+					@select="handleselect"
+					unique-opened
+					router
+					v-if="!collapsed">
+					<template
+						v-for="(item, index) in $router.options.routes"
+						v-if="!item.hidden">
+						<el-submenu
+							:index="index+''"
+							v-if="!item.leaf">
+							<template slot="title">
+								<i :class="item.iconCls"></i>
+								{{ item.name }}
+							</template>
+							<el-menu-item
+								v-for="child in item.children"
+								:index="child.path"
+								v-if="!child.hidden"
+								:key="child.path">
+								{{ child.name }}
+							</el-menu-item>
 						</el-submenu>
-						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+						<el-menu-item
+							v-if="item.leaf && item.children.length"
+							:index="item.children[0].path">
+							<i :class="item.iconCls"></i>
+							{{ item.children[0].name }}
+						</el-menu-item>
 					</template>
 				</el-menu>
 
 				<!--导航菜单-折叠后-->
-				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-					<li v-for="(item, index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
+				<ul
+					class="el-menu el-menu-vertical-demo collapsed"
+					v-if="collapsed"
+					ref="menuCollapsed">
+					<li
+						v-for="(item, index) in $router.options.routes"
+						v-if="!item.hidden"
+						class="el-submenu item"
+						:key="index">
 						<template v-if="!item.leaf">
-							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
-								<li v-for="child in item.children" v-if="!child.hidden" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
+							<div
+								class="el-submenu__title"
+								style="padding-left: 20px;"
+								@mouseover="showMenu(index,true)"
+								@mouseout="showMenu(index,false)">
+								<i :class="item.iconCls"></i>
+							</div>
+							<ul
+								class="el-menu submenu"
+								:class="'submenu-hook-' + index"
+								@mouseover="showMenu(index,true)"
+								@mouseout="showMenu(index,false)">
+								<li
+									v-for="(child, index) in item.children"
+									v-if="!child.hidden"
+									class="el-menu-item"
+									style="padding-left: 40px;"
+									:class="$route.path==child.path?'is-active':''"
+									@click="$router.push(child.path)"
+									:key="index">
+									{{ child.name }}
+								</li>
 							</ul>
 						</template>
 						<template v-else>
 							<li class="el-submenu">
-								<div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
+								<div
+									class="el-submenu__title el-menu-item"
+									style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;"
+									:class="$route.path == item.children[0].path ? 'is-active' : ''"
+									@click="$router.push(item.children[0].path)">
+									<i :class="item.iconCls"></i>
+								</div>
 							</li>
 						</template>
 					</li>
 				</ul>
 			</aside>
-			<section class="content-container">
-				<div class="grid-content bg-purple-light">
-					<el-col :span="24" class="breadcrumb-container">
-						<strong class="title">{{$route.name}}</strong>
-						<el-breadcrumb separator="/" class="breadcrumb-inner">
+			<section
+				class="content-container">
+				<div
+					class="grid-content bg-purple-light">
+					<el-col
+						:span="24"
+						class="breadcrumb-container">
+						<strong class="title">
+							{{ $route.name }}
+						</strong>
+						<el-breadcrumb
+							separator="/"
+							class="breadcrumb-inner">
 
 							<!-- 路由记录生成面包屑 -->
-							<el-breadcrumb-item v-for="item in $route.matched">
+							<el-breadcrumb-item
+								v-for="item in $route.matched"
+								:key="item.path">
 								{{ item.name }}
 							</el-breadcrumb-item>
 						</el-breadcrumb>
 					</el-col>
-					<el-col :span="24" class="content-wrapper">
-						<transition name="fade" mode="out-in">
+					<el-col
+						:span="24"
+						class="content-wrapper">
+						<transition
+							name="fade"
+							mode="out-in">
 							<router-view></router-view>
 						</transition>
 					</el-col>
